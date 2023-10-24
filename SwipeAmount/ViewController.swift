63e15eda94b34af4881d6184a50c46eb8caa05bd
halil dikisli <halil851@gmail.com>
2023-10-24 18:05:44 +0300

    Initial Commit

---
+++ SwipeAmount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeAmount
+//
+//  Created by halil dikişli on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
